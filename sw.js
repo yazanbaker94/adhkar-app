b1aca53a5facf1f09ba5429cb431907335bb5896
yazanbaker94 <79137633+yazanbaker94@users.noreply.github.com>
2025-05-01 18:39:21 +0300

    qibla fix android 4

---
+++ sw.js
@@ -0,0 +1,36 @@
+self.addEventListener('push', function(event) {
+    if (event.data) {
+        const data = event.data.json();
+        const options = {
+            body: data.body,
+            icon: 'icon1.png',
+            badge: 'icon1.png',
+            vibrate: [200, 100, 200],
+            data: {
+                url: data.url
+            }
+        };
+
+        event.waitUntil(
+            self.registration.showNotification(data.title, options)
+        );
+    }
+});
+
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    
+    if (event.notification.data && event.notification.data.url) {
+        event.waitUntil(
+            clients.openWindow(event.notification.data.url)
+        );
+    }
+});
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(self.clients.claim());
+}); 
